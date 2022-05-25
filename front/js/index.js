972298b68a02f062a7617100a04532733c540f5e
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-05-25 07:58:41 +0200

    [ADD] the loop to print all product in the ul in the fetch, for now there is only one li displayed and we only see the name of the product

--- front/js/index.js
+++ front/js/index.js
@@ -1,18 +1,29 @@
-fetch("http://localhost:3000/api/products")
+var myRequest = new Request("http://localhost:3000/api/products");
+let ul = document.createElement("ul");
+
+document.getElementById("items").appendChild(ul);
+ul.classList.add("items__list");
+
+fetch(myRequest)
     .then(function(response) {
         if(response.ok) {
             return response.json();
         }
+        else {
+            console.log('Mauvaise réponse du serveur !');
+        }
     })
 .then(function(value) {
     console.log(value);
+    for (let index = 0; index < arguments.length; index++) {
+        const element = value[index];
+        let li = document.createElement("li");
+        ul.appendChild(li);
+        li.classList.add("items__item");
+        li.innerHTML = value[index].name;
+        console.log(element);
+    }
 })
 .catch(function(error) {
     // Une erreur est survenue
 });
-
-let ul = document.createElement("ul");
-
-document.getElementById("items").appendChild(ul);
-ul.innerHTML = "<h1>caca</h1>";
-ul.classList.add("items__list");
