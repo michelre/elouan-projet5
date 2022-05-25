1f93c7612f724cc0c51e03ddea426b412de262e6
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-05-25 23:10:04 +0200

    [ADD] the href to the <a> tag to the productpage

--- front/js/index.js
+++ front/js/index.js
@@ -13,11 +13,10 @@ fetch(myRequest)
     for (let index = 0; index < value.length; index++) {
             const element = value[index];
             let a = document.createElement("a");
-            a.classList.add("product");
+            a.href = "product.html?id=" + element._id;
             document.getElementById("items").appendChild(a);
             let article = document.createElement("article");
             a.appendChild(article);
-            article.classList.add("product__article");
             article.innerHTML = `<img src="${element.imageUrl}"></img><h3>${element.name}</h3><p>${element.description}</p>`;
             console.log(element);
             console.log(index);
