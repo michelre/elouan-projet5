d9c0adef2ded58e03aac6287a3685a222620bf85
Rémi Michel <remi.michel38@gmail.com>
2022-05-31 19:05:45 +0200

    feat(): Session du 31/05

---
+++ front/js/cart.js
@@ -0,0 +1,7 @@
+/**
+ *
+ * TODO
+ *  - Récupérer le localStorage pour afficher le détail des produits
+ *  - Calcul le total global
+ *  - On ajoute les évènements permettant de vider le panier et de supprimer chaque produit
+ **/
