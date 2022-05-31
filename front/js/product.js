98de2348380340cb32ded2934e1b20c39b19991a
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-05-31 20:24:07 +0200

    [FIX] the js files and html files after the sessions of the day

--- front/js/product.js
+++ front/js/product.js
@@ -1,43 +1,49 @@
-const urlParams = new URLSearchParams(window.location.search);
-const pageId = urlParams.get('id');
-id = pageId;
+function fetchProduct(id){
+    return fetch(`http://localhost:3000/api/products/${id}`)
+        .then(res => res.json())
+}
 
-fetch(`http://localhost:3000/api/products/${id}`)
-    .then(function(response) {
-        if(response.ok) {
-            return response.json();
-        }
-        else {
-            console.log('Mauvaise réponse du serveur !');
-        }
-    })
-.then(function(value) {
-    console.log(value);
-    document.getElementById("title").innerText = value.name;
+function displayProduct(product){
+    const value = product;
+    document.querySelector("#title").innerText = value.name;
     document.getElementById("description").innerText = value.description;
     document.getElementById("price").innerText = value.price;
     let img = document.createElement("img");
     document.querySelector(".item__img").appendChild(img);
     img.src = value.imageUrl;
     for (let index = 0; index < value.colors.length; index++) {
-            document.getElementById("colors").innerHTML += `<option value="${value.colors[index]}">${value.colors[index]}</option>`;
+        document.getElementById("colors").innerHTML +=
+            `<option value="${value.colors[index]}">${value.colors[index]}</option>`;
     }
-let itemNumber = document.getElementById("quantity");
-// itemNumber.addEventListener("input", function() {});
-const itemColor = document.getElementById("colors").getElementsByTagName('option')[value.colors];
+    let itemNumber = document.getElementById("quantity");
+    // itemNumber.addEventListener("input", function() {});
+    const itemColor = document
+        .getElementById("colors")
+        .getElementsByTagName('option')[value.colors];
 
-    function send() {
-    fetch(`http://localhost:3000/api/products/${id}`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(value, itemColor, itemNumber)
-    });
-    console.log(value, itemColor, itemNumber);
 }
 
-const buttonCart = document.getElementById("addToCart");
-buttonCart.addEventListener("click", send);
-})
+function addToCart(product){
+
+
+// Mettre à jour le localStorage
+//localStorage.setItem('cart', JSON.stringify([])) // On stock une chaine
+
+// Récupération du localStorage
+//JSON.parse(localStorage.getItem('cart')) // On récupère un tableau
+}
+
+function initEvents(product){
+    const buttonCart = document.querySelector("#addToCart");
+    buttonCart.addEventListener("click", () => addToCart(product));
+}
+
+async function init(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    const product = await fetchProduct(id)
+    displayProduct(product)
+    initEvents(product)
+}
+
+init();
