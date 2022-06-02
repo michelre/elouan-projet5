4d8343c0b139ec0eb40b58959cd9707d71e02755
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-06-02 18:10:31 +0200

    [FIX] the infinite loop problem, add JSON.parse to cart, can print the array correctly in the console, the loop print it 4 time but it's ok for now, still have undefined on all property in the inner.html, working on it

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,36 +9,22 @@
  *  - On ajoute les évènements permettant de vider le panier et de supprimer chaque produit
  **/
 
- const cart = localStorage.getItem('cart');
-
-
-function fetchProduct () {
-    for (let i of cart) {
-        fetch('http://localhost:3000/api/products/'+ i.id)
-        .then(response => response.json())
-        .then(product => {
-            console.log(cart);
-        })
-}
-}
-
-fetchProduct();
 
 function displayProduct (cart) {
     document.querySelector('#cart__items').innerHTML = `
-    <article data-id="${item.product.id}" data-color="${item.itemColor}">
+    <article data-id="${cart._id}" data-color="${cart.itemColor}">
     <div class="cart__item__img">
-        <img src="${item.product.imageUrl}" alt="${item.product.name}">
+        <img src="${cart.imageUrl}" alt="${cart.name}">
     </div>
     <div class="cart__item__content">
         <div class="cart__item__content__description">
-            <h2>${item.product.name}</h2>
-            <p>${item.itemColor}</p>
-            <p>${item.product.price}</p>
+            <h2>${cart.name}</h2>
+            <p>${cart.itemColor}</p>
+            <p>${cart.price}</p>
         </div>
         <div class="cart__item__content__setting">
             <div class="cart__item__content__setting__quantity">
-                <p>Qté : ${item.itemNumber}</p>
+                <p>Qté : ${cart.itemNumber}</p>
                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42"
             </div>
             <div class="cart__item__content__settings__delete">
@@ -49,6 +35,21 @@ function displayProduct (cart) {
     </article>`;
 }
 
+
+async function init() {
+    let cart = localStorage.getItem('cart');
+    cart = JSON.parse(cart);
+        for (let i of cart) {
+            fetch('http://localhost:3000/api/products/'+ i.id)
+            .then(response => response.json())
+            .then(product => {
+                displayProduct(product)
+                console.log(cart)
+            })
+}
+}
+
+init();
 
 
 /**const cart = localStorage.getItem('cart');
