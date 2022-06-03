a02084242d903c82c25d68d3fa97335610831b49
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-06-03 20:41:49 +0200

    [FIX] the fetch request, print only one item but that's work

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,17 +10,64 @@
  **/
 
 
-function fetchProduct (Carts) {
-    for (i in Object.keys(Carts)) {
-        fetch(`http://localhost:3000/api/products/${Object.keys(Carts_id)[i]}`)
+ function fetchProduct (Cart) {
+    for (i in Object.keys(Cart)) {
+        fetch(`http://localhost:3000/api/products/${Object.values(Cart)[i].product._id}`)
         .then(response => response.json())
         .then(product => {
-            displayProduct(product)
+            document.querySelector('#cart__items').innerHTML = `
+            <article data-id="${product._id}" data-color="${product.itemColor}">
+            <div class="cart__item__img">
+                <img src="${product.imageUrl}" alt="${product.name}">
+            </div>
+            <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                    <h2>${product.name}</h2>
+                    <p>${Object.values(Cart)[i].itemColor}</p>
+                    <p>${product.price}</p>
+                </div>
+                <div class="cart__item__content__setting">
+                    <div class="cart__item__content__setting__quantity">
+                        <p>Qté : ${Object.values(Cart)[i].itemNumber}</p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42"
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                    </div>
+                </div>
+            </div>
+            </article>`;
         })
 }
 }
 
-function displayProduct (Carts) {
+async function init() {
+    let cart = localStorage.getItem('cart');
+    let Cart = JSON.parse(cart);
+    let fetch = await fetchProduct(Cart);
+    console.log(Cart);
+}
+
+init();
+
+
+
+
+
+
+
+
+/**const cart = localStorage.getItem('cart');
+
+for (let i of cart) {
+  fetch('http://localhost:3000/api/products/'+ i.id)
+  .then(response => response.json())
+  .then(product => {
+        // .innerHTML += `${product._id} ......`
+  })
+} */
+
+/**function displayProduct (product) {
     document.querySelector('#cart__items').innerHTML = `
     <article data-id="${product._id}" data-color="${product.itemColor}">
     <div class="cart__item__img">
@@ -29,7 +76,7 @@ function displayProduct (Carts) {
     <div class="cart__item__content">
         <div class="cart__item__content__description">
             <h2>${product.name}</h2>
-            <p>${product.itemColor}</p>
+            <p>${Object.values(cart)[i].itemColor}</p>
             <p>${product.price}</p>
         </div>
         <div class="cart__item__content__setting">
@@ -43,27 +90,4 @@ function displayProduct (Carts) {
         </div>
     </div>
     </article>`;
-}
-
-
-async function init() {
-    let Cart = localStorage.getItem('cart');
-    let Carts = JSON.parse(Cart);
-    let Carts_id = Carts.find(Cart => Cart.product._id === product._id);
-    let product = await fetchProduct(Carts_id);
-    fetchProduct(Carts);
-    console.log(Carts);
-}
-
-init();
-
-
-/**const cart = localStorage.getItem('cart');
-
-for (let i of cart) {
-  fetch('http://localhost:3000/api/products/'+ i.id)
-  .then(response => response.json())
-  .then(product => {
-        // .innerHTML += `${product._id} ......`
-  })
-} */
+}**/
