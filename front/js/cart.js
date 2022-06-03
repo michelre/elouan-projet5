977cfc0860b1ddc9451ac9c440892b1e5421d9dc
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-06-03 19:37:09 +0200

    [ADD] lines that don't work, i delete everything and restart

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,21 +10,31 @@
  **/
 
 
-function displayProduct (cart) {
+function fetchProduct (Carts) {
+    for (i in Object.keys(Carts)) {
+        fetch(`http://localhost:3000/api/products/${Object.keys(Carts_id)[i]}`)
+        .then(response => response.json())
+        .then(product => {
+            displayProduct(product)
+        })
+}
+}
+
+function displayProduct (Carts) {
     document.querySelector('#cart__items').innerHTML = `
-    <article data-id="${cart._id}" data-color="${cart.itemColor}">
+    <article data-id="${product._id}" data-color="${product.itemColor}">
     <div class="cart__item__img">
-        <img src="${cart.imageUrl}" alt="${cart.name}">
+        <img src="${product.imageUrl}" alt="${product.name}">
     </div>
     <div class="cart__item__content">
         <div class="cart__item__content__description">
-            <h2>${cart.name}</h2>
-            <p>${cart.itemColor}</p>
-            <p>${cart.price}</p>
+            <h2>${product.name}</h2>
+            <p>${product.itemColor}</p>
+            <p>${product.price}</p>
         </div>
         <div class="cart__item__content__setting">
             <div class="cart__item__content__setting__quantity">
-                <p>Qté : ${cart.itemNumber}</p>
+                <p>Qté : ${product.itemNumber}</p>
                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42"
             </div>
             <div class="cart__item__content__settings__delete">
@@ -37,16 +47,12 @@ function displayProduct (cart) {
 
 
 async function init() {
-    let cart = localStorage.getItem('cart');
-    cart = JSON.parse(cart);
-        for (let i of cart) {
-            fetch('http://localhost:3000/api/products/'+ i.id)
-            .then(response => response.json())
-            .then(product => {
-                displayProduct(product)
-                console.log(cart)
-            })
-}
+    let Cart = localStorage.getItem('cart');
+    let Carts = JSON.parse(Cart);
+    let Carts_id = Carts.find(Cart => Cart.product._id === product._id);
+    let product = await fetchProduct(Carts_id);
+    fetchProduct(Carts);
+    console.log(Carts);
 }
 
 init();
