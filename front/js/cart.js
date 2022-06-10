961b9bb37059a9ddfdb7098355fc947f08bbea4e
elouandacostapeixoto@gmail.com <elouandacostapeixoto@gmail.com>
2022-06-10 17:19:26 +0200

    [ADD] the function supprButton, it work perfectly

--- front/js/cart.js
+++ front/js/cart.js
@@ -55,23 +55,28 @@ function changeQuantity () {
             let id = quantity.dataset.id;
             id = cart.find(item => item.product._id === id);
             productQuantity = id.itemNumber;
-
             productQuantity = e.target.value;
-            //itemNumber = productQuantity
             id.itemNumber = productQuantity;
-            console.log(cart);
             localStorage.setItem('cart', JSON.stringify(cart));
         })
     })
-
 }
 
 function deleteButton () {
-    let supprButton = document.querySelector('.deleteItem');
-    supprButton.addEventListener('click', function (e) {
-        e.preventDefault();
-        
+    let supprButtons = document.querySelectorAll('.deleteItem')
+    supprButtons.forEach((supprButton) => {
+        supprButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            const retrievedCart = localStorage.getItem('cart');
+            let cart = JSON.parse(retrievedCart) || [];
+            const suppr = supprButton.closest('.cart__item');
+            let id = suppr.dataset.id;
+            id = cart.find(item => item.product._id === id);
+            cart.splice(cart.indexOf(id), 1);
+            console.log(id);
+            localStorage.setItem('cart', JSON.stringify(cart));
     })
+})
 }
 
 async function init() {
@@ -79,6 +84,7 @@ async function init() {
     let Cart = JSON.parse(cart);
     let fetch = await fetchProduct(Cart);
     changeQuantity();
+    deleteButton();
 }
 
 init();
@@ -97,86 +103,3 @@ init();
     },
     products: ['_ae98..', '...']
 }*/
-
-
-
-
-
-
-
-
-/**function fetchProducts (cart) {
-    return Promise.all(cart.map(product => __fetchProduct(product)))
-}*/
-
-/**function __fetchProduct(data){
-    return fetch(`http://localhost:3000/api/products/${data.product._id}`)
-        .then(response => response.json())
-        .then(product => {
-            document
-                .querySelector('#cart__items')
-                .innerHTML += `
-            <article class="cart__item" data-id="${product._id}" data-color="${data.itemColor}">
-            <div class="cart__item__img">
-                <img src="${product.imageUrl}" alt="${product.name}">
-            </div>
-            <div class="cart__item__content">
-                <div class="cart__item__content__description">
-                    <h2>${product.name}</h2>
-                    <p>${data.itemColor}</p>
-                    <p>${product.price}</p>
-                </div>
-                <div class="cart__item__content__setting">
-                    <div class="cart__item__content__setting__quantity">
-                        <p>Qté : </p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${data.itemNumber}"
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                        <p class="deleteItem">Supprimer</p>
-                    </div>
-                </div>
-            </div>
-            </article>`;
-        })
-}*/
-
-
-
-
-
-
-
-/**const cart = localStorage.getItem('cart');
-
-for (let i of cart) {
-  fetch('http://localhost:3000/api/products/'+ i.id)
-  .then(response => response.json())
-  .then(product => {
-        // .innerHTML += `${product._id} ......`
-  })
-} */
-
-/**function displayProduct (product) {
-    document.querySelector('#cart__items').innerHTML = `
-    <article data-id="${product._id}" data-color="${product.itemColor}">
-    <div class="cart__item__img">
-        <img src="${product.imageUrl}" alt="${product.name}">
-    </div>
-    <div class="cart__item__content">
-        <div class="cart__item__content__description">
-            <h2>${product.name}</h2>
-            <p>${Object.values(cart)[i].itemColor}</p>
-            <p>${product.price}</p>
-        </div>
-        <div class="cart__item__content__setting">
-            <div class="cart__item__content__setting__quantity">
-                <p>Qté : ${product.itemNumber}</p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42"
-            </div>
-            <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
-            </div>
-        </div>
-    </div>
-    </article>`;
-}**/
